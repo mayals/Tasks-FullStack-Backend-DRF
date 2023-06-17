c22c41c321c8eae681d99f87aa9cbf45cf80737e
mayals <developer.web.dj@gmail.com>
2023-06-17 17:14:31 +0400

    config ORS_ALLOWED_ORIGINS

--- src/b_proj/settings.py
+++ src/b_proj/settings.py
@@ -131,3 +131,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CORS_ALLOWED_ORIGINS = [
+    "https://example.com",
+    "https://sub.example.com",
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
